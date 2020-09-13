33eebc5508045282a6f266cea49707811f699438
Felix Böhm <me@feedic.com>
2020-09-13 13:47:52 -0700

    docs: Update documentation for exported values

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-/** Types of elements found in the DOM */
+/** Types of elements found in htmlparser2's DOM */
 export const enum ElementType {
-    Text = "text", //Text
-    Directive = "directive", //<? ... ?>
-    Comment = "comment", //<!-- ... -->
-    Script = "script", //<script> tags
-    Style = "style", //<style> tags
-    Tag = "tag", //Any tag
-    CDATA = "cdata", //<![CDATA[ ... ]]>
+    /** Type for Text */
+    Text = "text",
+    /** Type for <? ... ?> */
+    Directive = "directive",
+    /** Type for <!-- ... --> */
+    Comment = "comment",
+    /** Type for <script> tags */
+    Script = "script",
+    /** Type for <style> tags */
+    Style = "style",
+    /** Type for Any tag */
+    Tag = "tag",
+    /** Type for <![CDATA[ ... ]]> */
+    CDATA = "cdata",
+    /** Type for <!doctype ...> */
     Doctype = "doctype",
 }
 
@@ -24,11 +32,19 @@ export function isTag(elem: { type: ElementType }): boolean {
 }
 
 // Exports for backwards compatibility
-export const Text = ElementType.Text; //Text
-export const Directive = ElementType.Directive; //<? ... ?>
-export const Comment = ElementType.Comment; //<!-- ... -->
-export const Script = ElementType.Script; //<script> tags
-export const Style = ElementType.Style; //<style> tags
-export const Tag = ElementType.Tag; //Any tag
-export const CDATA = ElementType.CDATA; //<![CDATA[ ... ]]>
+/** Type for Text */
+export const Text = ElementType.Text;
+/** Type for <? ... ?> */
+export const Directive = ElementType.Directive;
+/** Type for <!-- ... --> */
+export const Comment = ElementType.Comment;
+/** Type for <script> tags */
+export const Script = ElementType.Script;
+/** Type for <style> tags */
+export const Style = ElementType.Style;
+/** Type for Any tag */
+export const Tag = ElementType.Tag;
+/** Type for <![CDATA[ ... ]]> */
+export const CDATA = ElementType.CDATA;
+/** Type for <!doctype ...> */
 export const Doctype = ElementType.Doctype;
