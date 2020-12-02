6da046213b2db08d9c1c28fb5f30c7b00d06e9b2
Felix Boehm <me@feedic.com>
2020-12-01 16:05:56 -0800

    feat: Add `root` type
    This type has been used for a long time by cheerio, let's make it official.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /** Types of elements found in htmlparser2's DOM */
 export const enum ElementType {
+    /** Type for the root element of a document */
+    Root = "root",
     /** Type for Text */
     Text = "text",
     /** Type for <? ... ?> */
@@ -32,6 +34,8 @@ export function isTag(elem: { type: ElementType }): boolean {
 }
 
 // Exports for backwards compatibility
+/** Type for the root element of a document */
+export const Root = ElementType.Root;
 /** Type for Text */
 export const Text = ElementType.Text;
 /** Type for <? ... ?> */
